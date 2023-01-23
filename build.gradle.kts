69c5ea55489f57b641c579f5393433da42d26016
smarkwal <stephan@markwalder.net>
2023-01-23 14:58:17 +0100

    Bump AssertJ to 3.24.2, JUnit to 5.9.2, and Tomcat to 9.0.71.

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,20 +77,21 @@ dependencies {
 
     // main dependencies -------------------------------------------------------
 
-    api("org.apache.tomcat:tomcat-catalina:9.0.70")
+    api("org.apache.tomcat:tomcat-catalina:9.0.71")
 
     // test dependencies -------------------------------------------------------
 
     // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 
     // Mockito
+    // Note: Mockito 5 is not compatible with Java 8.
     testImplementation("org.mockito:mockito-core:4.11.0")
     testImplementation("org.mockito:mockito-junit-jupiter:4.11.0")
 
     // AssertJ
-    testImplementation("org.assertj:assertj-core:3.24.1")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 
     // Testcontainers
     testImplementation("org.testcontainers:testcontainers:1.17.6")
